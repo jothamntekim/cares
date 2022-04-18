b8b666617e5304b04ed89e93dc054b0e72057e2f
Ntekim-Jotham <jothamntekim@gmail.com>
2022-04-18 20:09:52 +0100

    gh-pages

---
+++ src/components/main-utils/nav.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="nav">
+        <h1>Dashboard</h1>
+
+        <div class="date">
+            <input type="date">
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "Nav"
+}
+</script>
+<style scoped>
+    .date{
+       display: inline-block;
+       background: var(--color-light);
+       border-radius: var(--border-radius-1);
+       margin-left: 1rem;
+       padding: 0.5rem 1.5rem;
+    }
+
+    .date input[type="date"] {
+       background: transparent;
+       color: var(--color-dark);
+    }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,59 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import AddPharmacy from '../components/main-utils/home-content/create.vue'
+import DashboardIndex from '../components/main-utils/home-content/index.vue'
+import PillIndex from '../components/main-utils/pill-shop/index.vue'
+import AddDrug from '../components/main-utils/pill-shop/add-drug.vue'
+import AddPill from '../components/main-utils/pill-box/add-pill.vue'
+import PillBox from '../components/main-utils/pill-box/index.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  // {
+  //   path: '/add-pharmacy',
+  //   name: 'pharmacy',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (pharmacy.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "pharmacy" */ '../components/main-utils/home-content/create.vue')
+  // },
+  {
+    path: '/',
+    name: 'Dashboard',
+    component: DashboardIndex 
+  },
+  {
+    path: '/add-pharmacy',
+    name: 'Pharmacy',
+    component: AddPharmacy
+  },
+  {
+    path: '/pill-shop',
+    name: 'Pill',
+    component: PillIndex
+  },
+  {
+    path: '/add-drug',
+    name: 'Pill',
+    component: AddDrug
+  },
+  {
+    path: '/pill-box',
+    name: 'Pillbox',
+    component: PillBox
+  },
+  {
+    path: '/add-pill',
+    name: 'Pill',
+    component: AddPill
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
